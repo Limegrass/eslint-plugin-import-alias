5b28ba5a5536c6200445296fca8b80ec957e5834
limegrass <james@niis.me>
2024-03-11 02:15:00 -0700

    feat: add `recommended` config
    Added the configuration suggested in the primary README.
    While there may be some argument that barrel files
    should be supported as a part of the README,
    the current implementation does not have great support
    to limit it only to those exports.
    
    Splitting out the rules would be a good step towards that direction;
    however, that would require some more work that will be left to
    a future change.
    
    In the mean time, the recommended config is kept simple
    to just require all imports be absolute.
    
    Closes #12

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import { importAliasRule } from "#src/rules/import-alias";
+import { type ESLint } from "eslint";
 
 export const rules = {
     "import-alias": importAliasRule,
+};
+
+export const configs: Record<string, ESLint.ConfigData> = {
+    recommended: {
+        plugins: ["@limegrass/import-alias"],
+        rules: {
+            "@limegrass/import-alias/import-alias": "error",
+        },
+    },
 };
