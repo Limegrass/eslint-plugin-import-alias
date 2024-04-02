a286334c5a43722786b1883c61fd5e1b3e5af625
limegrass <james@niis.me>
2024-04-02 02:58:59 -0700

    docs: update nested-config example to showcase path inheritance using `extends`
    Simpler TSConfig redefinition is already showcased with the TSConfig in
    the `src` path of this example. This gives a bit greater coverage both
    in terms of testing the plugin and in documentation.

--- examples/nested-configs/test/index.ts
+++ examples/nested-configs/test/index.ts
@@ -1,3 +1,3 @@
-import { bar } from "./foo"; // warn: import ./foo can be written as #test/foo
+import { bar } from "./foo"; // warn: import ./foo can be written as #root/test/foo
 
 export const baz = bar + "baz";
